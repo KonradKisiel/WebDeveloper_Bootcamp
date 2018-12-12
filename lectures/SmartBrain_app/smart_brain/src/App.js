92b7ef6a8c88349ca260817e1b3778f25a934a25
KonradKisiel <konrad.kisiel.webmaster@gmail.com>
2018-12-12 21:52:23 +0100

    image recognition api

--- lectures/SmartBrain_app/smart_brain/src/App.js
+++ lectures/SmartBrain_app/smart_brain/src/App.js
@@ -1,35 +1,75 @@
 import React, { Component } from 'react';
+import Particles from 'react-particles-js';
+import Clarifai from 'clarifai';
 import Navigation from './components/Navigation/Navigation';
+import FaceRecognition from './components/FaceRecognition/FaceRecognition';
 import Logo from './components/Logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import './App.css';
 import Rank from './components/Rank/Rank';
-import Particles from 'react-particles-js';
+
+// initialize with your api key. This will also work in your browser via http://browserify.org/
+const app = new Clarifai.App({
+ apiKey: 'secret'
+});
 
 const particleOptions = {
-    particles: {
-      number: {
-        value: 400,
-        density: {
-          enable: true,
-          vaue_area: 800
-        }
+  particles: {
+    number: {
+      //add 00
+      value: 400,
+      density: {
+        enable: true,
+        vaue_area: 800
       }
     }
+  }
 }
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      input: '',
+      imageUrl: ''
+    }
+  }
+
+  onInputChange = event => {
+   this.setState({input: event.target.value});
+  }
+
+  onButtonSubmit = () => {
+    {/* setting app state */}
+    this.setState({imageUrl: this.state.input})
+    app.models.predict(
+        Clarifai.FACE_DETECT_MODEL, 
+        this.state.input)
+      .then(
+      function (response) {
+        console.log(response.outputs[0].data.regions[0].region_info.bounding_box);
+      },
+      function (err) {
+        // there was an error
+      }
+    );
+  }
+
   render() {
     return (
       <div className="App">
         <Particles className="particles" params={particleOptions} />
         <Navigation />
         <Logo />
+        <br></br>
         <Rank />
-        <ImageLinkForm />
-        { /*      
-        <FaceRecognition /> 
-        */ }
+        <ImageLinkForm
+          onInputChange={this.onInputChange}
+          onButtonSubmit={this.onButtonSubmit}
+        />     
+        <br></br>
+        {/* use data from state (set in onButtonSubmit) */}
+        <FaceRecognition imageUrl={this.state.imageUrl} />
       </div>
     );
   }
--- lectures/SmartBrain_app/smart_brain/src/components/Logo/Logo.js
+++ lectures/SmartBrain_app/smart_brain/src/components/Logo/Logo.js
@@ -2,12 +2,14 @@ import React from 'react'
 import Tilt from 'react-tilt'
 import './Logo.css'
 import brain from './brain.png'
+import hall from './HAL9000.svg'
+
 
 const Logo = () => {
     return (
-        <div className='ma4 mt0'>
-            <Tilt className="Tilt br2 shadow-2" options={{ max: 55 }} style={{ height: 200, width: 200 }} >
-                <div className="Tilt-inner pa3"><img style={{paddingTop: '30px'}} src={brain} alt='logo'></img></div>
+        <div className='logo'>
+            <Tilt className="Tilt" options={{ max: 40 }} style={{ height: 80, width: 80 }} >
+                <div className="Tilt-inner pa3"><img className="shadow-2" style={{borderRadius: 100}} src={hall} alt='logo'></img></div>
             </Tilt>
         </div>
     );
