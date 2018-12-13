63d7fc60b84a721becefcae0b6404b68cd5f7551
KonradKisiel <konrad.kisiel.webmaster@gmail.com>
2018-12-13 14:29:52 +0100

    components update

--- exercises/Ex22_SPA_API/StarWarsApp/star_wars_app/src/components/SearchBox.js
+++ exercises/Ex22_SPA_API/StarWarsApp/star_wars_app/src/components/SearchBox.js
@@ -1,14 +1,24 @@
 import React from 'react'
 
-const SearchBox = ({ searchfield, searchChange }) => {
+const SearchBox = ({ searchChange, optionChange }) => {
     return (
-        <div id="Search-div">
-            <input
-                id="Search-input"
-                onChange={searchChange}
-                type="search"
-                placeholder="FIND..."
-            />
+        <div id="Search-box">
+            <form>
+                <select id="Search-select" name="select" onChange={optionChange}>
+                    <option value="people">CHARACTERS</option>
+                    <option value="planets">PLANETS</option>
+                    <option value="films">FILMS</option>
+                    <option value="species">SPECIES</option>
+                    <option value="vehicles">VEHICLES</option>
+                    <option value="starships">STARSHIPS</option>
+                </select>
+                <input
+                    id="Search-input"
+                    onChange={searchChange}
+                    type="search"
+                    placeholder="FIND..."
+                />
+            </form>
         </div>
     );
 }
--- exercises/Ex22_SPA_API/StarWarsApp/star_wars_app/src/containers/App.js
+++ exercises/Ex22_SPA_API/StarWarsApp/star_wars_app/src/containers/App.js
@@ -1,11 +1,8 @@
 import React, { Component } from 'react';
 import './App.css';
-import Select from '../components/Select';
 import SearchBox from '../components/SearchBox';
 import Content from '../components/Content';
 import Footer from '../components/Footer';
-import Scroll from '../components/Scroll';
-
 
 //let selecedValue = document.getElementById('Search-select').value;
 
@@ -14,7 +11,7 @@ class App extends Component {
     super();
     this.state = {
       cardsText: [],
-      select: '',
+      searchOption: 'people',
       searchfield: ''
     }
   }
@@ -30,31 +27,24 @@ class App extends Component {
   }
 
   onOptionChange = event => {
-    this.setState({ select: event.target.value })
+    this.setState({ searchOption: event.target.value })
     console.log(event.target.value)
   }
 
   render() {
-    const { cardsText, searchfield } = this.state;
+    const { cardsText, searchOption, searchfield } = this.state;
     const filteredCards = cardsText.filter(cardText => {
       return cardText.name.toLowerCase().includes(searchfield.toLowerCase());
     })
     return !cardsText.length ? <h1 id="loading" className="Sw-txt">Loading...</h1> : (
       <div className="App">
         <div id="App-top-bar">
-          <header className="App-header">
+          <header id="App-header">
             <h1 className="Sw-txt">Star Wars App</h1>
           </header>
-          <div id="Search-bar">
-            <form>
-              <Select optionChange={this.onOptionChange}/>
-              <SearchBox searchChange={this.onSearchChange} />
-            </form>
-          </div>
+            <SearchBox searchChange={this.onSearchChange} optionChange={this.onOptionChange} />
         </div>
-        <Scroll >
-          <Content cardsText={filteredCards} />
-        </Scroll>
+        <Content cardsText={filteredCards} />
         <Footer />
       </div>
     );
