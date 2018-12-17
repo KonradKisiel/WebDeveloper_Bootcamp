41b62227e49bbb435bfc43b45190443b6027b49c
KonradKisiel <konrad.kisiel.webmaster@gmail.com>
2018-12-17 16:41:00 +0100

    connect API to front-end

--- lectures/SmartBrain_app/smart_brain/src/components/Logo/Logo.js
+++ lectures/SmartBrain_app/smart_brain/src/components/Logo/Logo.js
@@ -1,14 +1,14 @@
 import React from 'react'
 import Tilt from 'react-tilt'
 import './Logo.css'
-import hall from './HAL9000.svg'
+import hal from './HAL9000.svg'
 
 
 const Logo = () => {
     return (
         <div className='logo'>
             <Tilt className="Tilt" options={{ max: -40 }} style={{ height: 80, width: 80 }} >
-                <div className="Tilt-inner pa3"><img className="shadow-2" style={{borderRadius: 100}} src={hall} alt='logo'></img></div>
+                <div className="Tilt-inner pa3"><img className="shadow-2" style={{borderRadius: 100}} src={hal} alt='logo'></img></div>
             </Tilt>
         </div>
     );
--- lectures/SmartBrain_app/smart_brain_api/server.js
+++ lectures/SmartBrain_app/smart_brain_api/server.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const bcrypt = require('bcrypt-nodejs');
+const cors = require('cors');
 
 const app = express();
 //if we want to compare incoming json with objects data we need to use bodyParser
 app.use(bodyParser.json());
+app.use(cors());
 
 const database = {
     users: [
@@ -23,16 +26,33 @@ const database = {
             entries: 0, //user score
             joined: new Date()
         },
+    ],
+    login: [
+        {
+            id:'987',
+            hash: '',
+            email: 'john@gmail.com'
+        }
     ]
 }
 
 app.get('/', (req, res)=>{
     res.send(database.users);
+
 })
 
 app.post('/signin', (req, res) => {
     //res.send('signin');
     //buildin (express) json method
+
+    // Load hash from your password DB.
+    bcrypt.compare("apples", '$2a$10$nnwC5KLT3nWIaM.rJlkvxuN0p4h7ulGdy/1mQTJj0vH1LA5iYJYtq', function(err, res) {
+        console.log('first guess', res);
+    });
+    bcrypt.compare("veggies", '$2a$10$nnwC5KLT3nWIaM.rJlkvxuN0p4h7ulGdy/1mQTJj0vH1LA5iYJYtq', function(err, res) {
+        console.log('second guess', res);
+    });
+
     if (req.body.email === database.users[0].email 
         && req.body.password === database.users[0].password){
         res.json('success');
@@ -43,6 +63,12 @@ app.post('/signin', (req, res) => {
 
 app.post('/register', (req, res)=>{
     const {email, name, password} = req.body;
+
+    bcrypt.hash(password, null, null, function(err, hash) {
+        // Store hash in your password DB.
+        console.log(hash);
+    });
+
     database.users.push({
             id: '125',
             name: name,
@@ -55,9 +81,34 @@ app.post('/register', (req, res)=>{
     res.json(database.users[database.users.length-1]);
 })
 
+app.get('/profile/:id', (req, res) => {
+    const {id} = req.params;
+    let found = false;
+    database.users.forEach(user=>{
+        if(user.id === id){
+            return res.json(user);
+        }
+    })
+    if (!found) res.status(400).json('not found');
+})
+
+app.put('/image', (req, res) =>{
+    const {id} = req.body;
+    let found = false;
+    database.users.forEach(user=>{
+        if(user.id === id){
+            found = true;
+            user.entries++;
+            return res.json(user.entries);
+        }
+    })
+    if (!found) res.status(400).json('not found');
+})
+
 app.listen(3000, ()=>{
     console.log('app is running on port 3000');
 })
+
 
 /*
 / --> res = this is working
@@ -65,5 +116,4 @@ app.listen(3000, ()=>{
 / register --> POST = user          //return new user object
 / profile/:userId --> GET = user
 / image --> PUT --> user score      //upadting a score
-
 */
